08514e07761c05bc1d60773316abf78b031bdf6b
gamingkenzo <111523835+gamingkenzo@users.noreply.github.com>
2022-08-25 16:53:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,7 +36,7 @@ global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
 
 //bot body 
-global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
+global.alivepic =`https://i.ibb.co/VTxVmF6/20220821-153441.jpg` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
 global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
@@ -44,19 +44,19 @@ global.antibad = "false" // To remove bad word senders
 global.antilink = "false" // To remove group link sharing peoples
 global.upsongvoice = "true" // bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
 
-global.owner = ['+94763867156']
-global.ownertag = '+94763867156' //your tag number
+global.owner = ['+94763867158']
+global.ownertag = '+94763867158' //your tag number
 global.botname = 'KENZO BOT - 𝘉𝘖𝘛' //ur bot name
 global.packname = "Bot KENZO" // sticker packname
 global.author = "𝙍𝙀𝘿╺ KENZO" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '[ ◉ ❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱ ]' //
 
 global.button = '☰ YOUTUBE  ☰' //Costomize A Button Name In Alive Message
 global.btnurl = 'https://youtube.com/channel/UCxqpAzaAecDfYOPywxUJoyw ' //Change Your Button's URL
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+94763867158'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
